f557b7ac83a1dc73a3508beb61b42c62fa13df45
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2025-06-15 11:21:55 +0300

    React card components

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Card(){
+    return (
+        <div className="card">
+            <img className = "card-image"src = "https://via.placeholder.com/150"alt = "Profile Picture "></img>
+            <h2 className="card-title">John Mwangi</h2>
+            <p className="card-text">I like to code...More specifically using 
+               python...dealing with machine learning problems is my hobby
+               </p>
+        </div>  
+
+    );
+}
+
+export default Card
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+    return (
+
+        <footer>
+            <p>&copy; {new Date().getFullYear()}Your websote name </p>
+        </footer>
+
+    );
+}
+
+
+export default Footer
